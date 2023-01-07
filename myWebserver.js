8bf8de8ffad921616af39f98ecd43358e8040100
Roland Wälzlein <rolandwalzlein@MacBook-Air-von-Roland.local>
2023-01-08 00:22:33 +0100

    changes

--- myWebserver.js
+++ myWebserver.js
@@ -14,7 +14,12 @@ let quotes = [
                                         "It is so plain and so obvious and so simple. " +
                                         "And yet, everybody rushes around in a great panic " +
                                         "as if it were necessary to achieve something beyond themselves."},
-    {id: 2, author: "Mooji", quote: "Feelings are just visitors, let them come and go."}  
+    {id: 2, author: "Mooji", quote: "Feelings are just visitors, let them come and go."},  
+    {id: 3, author: "Hermann Hesse", quote: "Wahrer Beruf für den Menschen ist nur, zu sich selbst zu kommen."},  
+    {id: 4, author: "Eckhart Tolle", quote: "Ich bin nicht meine Gedanken, Emotionen, Sinneseindrücke und Erfahrungen. "+
+                                "Ich bin nicht der Inhalt meines Lebens. Ich bin das Leben selbst. "+
+                                "Ich bin der Raum, in dem alle Dinge passieren. Ich bin Bewusstsein. " + 
+                                "Ich bin das jetzt. Ich bin."}  
 ]
 const requestListener = function (req, res){
     let {pathname} = url.parse(req.url, true);
@@ -39,8 +44,14 @@ const requestListener = function (req, res){
         });
     }else {
         res.setHeader('Content-Type', 'application/json;charset=utf-8');
-        return res.end(JSON.stringify(quotes));
+        let random = zufallszahl(0,3);
+        console.log("Zahl = " + random);
+        return res.end(JSON.stringify(quotes[random]));
     }
+}
+
+function zufallszahl(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const server = http.createServer(requestListener);
